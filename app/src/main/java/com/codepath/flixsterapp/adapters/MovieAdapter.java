73cb5ddb053ec0c9a4ab601861e97a14b7917fce
janetosawere7 <janetosawere7@gmail.com>
2020-10-02 19:15:32 -0400

    Implementation of Week 2 Assignment

--- app/src/main/java/com/codepath/flixsterapp/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/flixsterapp/adapters/MovieAdapter.java
@@ -1,13 +1,16 @@
 package com.codepath.flixsterapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,8 +18,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
+import com.codepath.flixsterapp.DetailActivity;
 import com.codepath.flixsterapp.R;
 import com.codepath.flixsterapp.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -43,7 +49,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     //Involves populating data into the item through holder
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Log.d("MovieAdapter", "onCreateViewHolder" + position);
+        Log.d("MovieAdapter", "onBindViewHolder" + position);
         //Get the movie at the passed in position
         Movie movie= movies.get(position);
         //Bind the movie data into the VH
@@ -59,6 +65,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -67,9 +74,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
@@ -90,6 +98,18 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
                     .load(imageUrl)
                     //.apply(new RequestOptions().override(120, 120))
                     .into(ivPoster);
+            //1.Register the click listener on the whole container
+            container.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    //2.Navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
+
+
         }
     }
 }
